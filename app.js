4e757ca1fd0999a8d3d987c17a7d42e2f78f4dc9
Jack <kamabas022@student.wethinkcode.co.za>
2023-05-04 14:08:03 +0200

    Adjust const to let

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 const ratingCard = document.querySelector('.rating-card');
 const thankYouCard = document.querySelector('.thank-you-card');
 const ratingForm = document.getElementById('rating-form');
-const alert = document.querySelector('.alert');
-const ratingBtns = document.querySelectorAll(".rating-numbers");
+const ratingBtns = document.querySelectorAll('.rating-numbers');
+let alertEl = document.querySelector('.alert');
 
 ratingBtns.forEach((btn) => {
-    btn.addEventListener('click',function(){
-        alert.classList.add("message");
-        alert.textContent = `You selected ${this.value} out of 5`
-    })
-})
+  btn.addEventListener('click', () => {
+    alertEl.classList.add('message');
+    alertEl.textContent = `You selected ${btn.value} out of 5`;
+  });
+});
 
-ratingForm.addEventListener('submit',function(e){
-    e.preventDefault();
-    ratingCard.classList.add('hidden');
-    thankYouCard.classList.remove('hidden');
-})
+ratingForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  ratingCard.classList.add('hidden');
+  thankYouCard.classList.remove('hidden');
+});
